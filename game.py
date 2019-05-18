7b79b2e047d45aa729edaaaad25c958583234778
todoroff <stoyansky@gmail.com>
2019-05-18 22:01:25 +0100

    sample game initialization

---
+++ game.py
@@ -0,0 +1,27 @@
+from board import Board
+import time
+
+# Initialize random state board with width=10 and height=5
+board = Board(10, 5)
+
+# Or create a custom state
+state = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+# and initialize a custom board
+board = Board.load_state(state)
+
+# Render to the command line
+while True:
+    board.render()
+    board.next_board_state()
+    time.sleep(1)
